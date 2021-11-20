db59bfff18545e146548df918e8096bcf85f5f6e
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2021-11-21 02:40:01 +0900

    Selecterを追加

---
+++ PersonalMap/Views/Common/SymbolSelecter.swift
@@ -0,0 +1,60 @@
+import SwiftUI
+
+protocol SymbolSelecterDelegate {
+    func symbolSelected(symbolName: String)
+}
+
+struct SymbolSelecter: View {
+    @Environment(\.presentationMode) var presentationMode
+    let columns: Int = 5
+    let delegate: SymbolSelecterDelegate?
+    
+    let systemNames: [[String]] = [
+        ["star.circle", "parkingsign.circle", "checkmark.circle", "hand.point.up.left", "photo.circle"],
+        ["fork.knife.circle", "takeoutbag.and.cup.and.straw", "car.circle","building.2.crop.circle", "location.north.line"],
+        ["circle.square", "mappin", "bolt.circle", "bolt.horizontal.circle", "wifi.circle"],
+        ["bolt.horizontal.circle", "wifi.circle", "hand.raised.circle", "exclamationmark.triangle", "point.3.connected.trianglepath.dotted"],
+        ["arrowtriangle.left.and.line.vertical.and.arrowtriangle.right", "0.circle","1.circle", "2.circle", "3.circle", "4.circle",],
+        [ "5.circle", "6.circle", "7.circle", "8.circle", "9.circle"],
+        [ "10.circle"],
+    ]
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading, spacing: 12) {
+                ForEach(0..<6) { i in
+                    HStack {
+                        ForEach(0..<5) { j in
+                            Button {
+                                delegate?.symbolSelected(symbolName: systemNames[i][j])
+                                presentationMode.wrappedValue.dismiss()
+                            } label: {
+                                Image(systemName: systemNames[i][j])
+                                    .resizable()
+                                    .frame(width: 40, height: 40)
+                            }
+                        }
+                    }
+                }
+                HStack(spacing: 12) {
+                    Button {
+                        delegate?.symbolSelected(symbolName: systemNames[6][0])
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Image(systemName: systemNames[6][0])
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("シンボルの選択")
+        }
+    }
+}
+
+struct SymbolSelecter_Previews: PreviewProvider {
+    static var previews: some View {
+        SymbolSelecter(delegate: nil)
+    }
+}
--- PersonalMap/Views/ListTab/AddMapPointObjectView.swift
+++ PersonalMap/Views/ListTab/AddMapPointObjectView.swift
@@ -1,58 +1,144 @@
 import SwiftUI
 
+enum AddMapPointObjectSheet: Identifiable {
+    case symbol
+    case location
+    case item
+    var id: Int {
+        hashValue
+    }
+}
+
 struct AddMapPointObjectView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-
+    
     let mapLayerId: UUID
     @State var objectName: String = ""
-    @State var showingSheet = false
+    @State private var symbolName: String = "star.circle"
+    @State var sheet: AddMapPointObjectSheet?
     @State var longitude: Double?
     @State var latitude: Double?
     
     var body: some View {
-        VStack {
-            Text("ポイントオブジェクト名")
-                .font(Font.system(size: 20).bold())
-                .padding(.top, 16)
-            TextField("オブジェクト名を入力してください", text: $objectName)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal)
-            
-            Button {
-                showingSheet = true
-            } label: {
-                Text("位置情報を設定")
-            }
-            
-            Text("緯度: \(latitude?.description ?? "???")")
-            Text("軽度: \(longitude?.description ?? "???")")
-            
-            
-            Button {
-                // point を保存
-                let mapObject: MapObject = .point(MapPoint(id: UUID(), imageName: "star.circle", isHidden: false, objectName: objectName, coordinate: Coordinate(latitude: latitude!, longitude: longitude!), infos: []))
-                let fileRepository = FileRepository()
-                try! fileRepository.initialize()
-                try! fileRepository.saveMapObject(mapObject: mapObject)
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("ラベル名")
+                    .font(Font.system(size: 20).bold())
+                    .padding(.top, 12)
+                TextField("ラベル名を入力してください", text: $objectName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                Text("シンボルの選択")
+                    .font(Font.system(size: 20).bold())
+                    .padding(.top, 12)
                 
-                // layer に追加
-                let mapLayer = try! fileRepository.getMapLayer(mapLayerId: mapLayerId)
-                let newMapLayer = MapLayer(
-                    id: mapLayer.id,
-                    layerName: mapLayer.layerName,
-                    mapObjectType: mapLayer.mapObjectType,
-                    objectIds: [mapObject.id] + mapLayer.objectIds)
-                try! fileRepository.saveMapLayer(mapLayer: newMapLayer)
-                presentationMode.wrappedValue.dismiss()
-            } label: {
-                Text("ポイントを追加")
+                HStack {
+                    Image(systemName: symbolName)
+                        .resizable()
+                        .foregroundColor(Color.blue)
+                        .frame(width: 52, height: 52)
+                        .padding(24)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                    Spacer()
+                    Button {
+                        sheet = .symbol
+                    } label: {
+                        Text("シンボルを設定")
+                    }
+                }
+
+                Text("位置情報を選択")
+                    .font(Font.system(size: 20).bold())
+                    .padding(.top, 12)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("緯度: \(latitude?.description ?? "???")")
+                        Text("経度: \(longitude?.description ?? "???")")
+                    }
+                    Spacer()
+                    Button {
+                        sheet = .location
+                    } label: {
+                        Text("位置情報を設定")
+                    }
+                }
+                
+                Text("項目の作成")
+                    .font(Font.system(size: 20).bold())
+                    .padding(.top, 12)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("- 綺麗な鉄塔")
+                        Text("- hyyps://swiswiswift.com")
+                            .foregroundColor(Color.blue)
+                        Text("- 写真1.png")
+                            .foregroundColor(Color.blue)
+                        Text("- file.pdf")
+                            .foregroundColor(Color.blue)
+                    }
+                    Spacer()
+                    Button {
+                        sheet = .item
+                    } label: {
+                        Text("項目を設定")
+                    }
+                }
+                
+                Spacer()
             }
+            .sheet(item: $sheet, onDismiss: {
+                
+            }, content: { item in
+                switch item {
+                case .symbol:
+                    SymbolSelecter(delegate: self)
+                case .location:
+                    PointLocationSelecter(delegate: self)
+                case .item:
+                    Text("item")
+                }
+            })
+            .padding(.horizontal, 16)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("ポイントの新規登録")
+            .navigationBarItems(
+                trailing:
+                Button(action: {
+                    savePoint()
+                }, label: {
+                    Text("登録")
+                        .font(Font.system(size: 16).bold())
+                })
+            )
         }
-        .sheet(isPresented: $showingSheet) {
-            // on dissmiss
-        } content: {
-            PointLocationSelecter(delegate: self)
-        }
+    }
+    
+    private func savePoint() {
+        let mapObject: MapObject = .point(MapPoint(id: UUID(), imageName: "star.circle", isHidden: false, objectName: objectName, coordinate: Coordinate(latitude: latitude!, longitude: longitude!), infos: []))
+        let fileRepository = FileRepository()
+        try! fileRepository.initialize()
+        try! fileRepository.saveMapObject(mapObject: mapObject)
+        
+        // layer に追加
+        let mapLayer = try! fileRepository.getMapLayer(mapLayerId: mapLayerId)
+        let newMapLayer = MapLayer(
+            id: mapLayer.id,
+            layerName: mapLayer.layerName,
+            mapObjectType: mapLayer.mapObjectType,
+            objectIds: [mapObject.id] + mapLayer.objectIds)
+        try! fileRepository.saveMapLayer(mapLayer: newMapLayer)
+        presentationMode.wrappedValue.dismiss()
+    }
+}
+
+extension AddMapPointObjectView: SymbolSelecterDelegate {
+    func symbolSelected(symbolName: String) {
+        self.symbolName = symbolName
     }
 }
 
